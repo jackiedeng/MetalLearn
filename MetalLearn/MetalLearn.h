b50223939c95cf034b5a078d654fe77ad1c0b94d
Jackie <63827088@qq.com>
2018-10-22 23:37:24 +0800

    Initial Commit

---
+++ MetalLearn/MetalLearn.h
@@ -0,0 +1,19 @@
+//
+//  MetalLearn.h
+//  MetalLearn
+//
+//  Created by Jackie on 2018/10/22.
+//  Copyright © 2018年 Jackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MetalLearn.
+FOUNDATION_EXPORT double MetalLearnVersionNumber;
+
+//! Project version string for MetalLearn.
+FOUNDATION_EXPORT const unsigned char MetalLearnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetalLearn/PublicHeader.h>
+
+
